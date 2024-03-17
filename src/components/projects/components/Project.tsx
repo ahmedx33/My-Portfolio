a19e01830f3bfc74e7823fa246f9ecc17a76921d
ahmedhany5 <faresahmed51857@gmail.com>
2024-03-17 15:36:47 +0200

    added vercel.json

--- src/components/projects/components/Project.tsx
+++ src/components/projects/components/Project.tsx
@@ -2,11 +2,8 @@ import "../styles/index.css"
 export default function Project() {
 
     return (
-        <div className="bg-[#48494A] p-8 w-[350px] h-[350px] relative  d-1a " onMouseMove={(e) => {
-
-            console.log(e)
-        }}>
-            <div className="bg-red-500 w-full h-full transition-all z-10"></div>
+        <div className="bg-[#48494A] p-8 w-[350px] h-[350px] relative  rounded-md">
+            <div className="bg-red-500 w-full h-full transition-all z-10 mb-5"></div>
             <div>Title</div>
         </div>
     )
