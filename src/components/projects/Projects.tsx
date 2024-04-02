a9f59a1cca085e7af939709365de48a2daa8128f
ahmedx33 <faresahmed51857@gmail.com>
2024-04-02 21:07:13 +0200

    buttons

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,13 +1,21 @@
 import Project from "./components/Project";
-import "./styles/index.css"
+import "./styles/index.css";
 
 export default function Projects() {
     return (
-        <div className="w-full h-screen py-14 px-24 gridaia ">
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+        <div className="py-14 px-24 w-full h-fit flex justify-center items-start relative">
+            <div className="absolute z-50 w-[80%] h-1/2">
+                <div className="absolute w-10 h-10 bg-red-500 right-0 translate-y-1/2 top-1/2 cursor-pointer">
+                    
+                </div>
+                <div className="absolute w-10 h-10 bg-red-500 left-0 translate-y-1/2 top-1/2 cursor-pointer"></div>
+            </div>
+            <div className="w-[1000px] overflow-y-hidden overflow-x-hidden flex items-center justify-center gap-5 ">
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+            </div>
         </div>
-    )
+    );
 }
--- src/components/projects/components/Project.tsx
+++ src/components/projects/components/Project.tsx
@@ -2,7 +2,7 @@ import "../styles/index.css"
 export default function Project() {
 
     return (
-        <div className="bg-[#48494A] p-8 w-[350px] h-[350px] relative  rounded-md">
+        <div className="bg-[#48494A] p-8 min-w-[350px] h-[350px] relative  rounded-md">
             <div className="bg-red-500 w-full h-full transition-all z-10 mb-5"></div>
             <div>Title</div>
         </div>
